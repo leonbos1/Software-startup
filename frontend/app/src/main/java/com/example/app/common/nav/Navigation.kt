f1e8c4daab36118f1761acc7965afdd9e80906e3
jasper-g <73937071+jasper-g@users.noreply.github.com>
2024-01-14 22:15:52 +0100

    fout gefixt
    had product overview per ongeluk verwijderd

--- frontend/app/src/main/java/com/example/app/common/nav/Navigation.kt
+++ frontend/app/src/main/java/com/example/app/common/nav/Navigation.kt
@@ -1,49 +1,44 @@
 package com.example.app.common.nav
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.app.common.Screens
-import com.example.app.presentation.views.screens.AccountScreen
-import com.example.app.presentation.views.screens.AddProductScreen
 import com.example.app.presentation.views.screens.ProductDetailsScreen
 import com.example.app.presentation.views.screens.ProductOverviewScreen
+import com.example.app.presentation.views.screens.AccountScreen
+import com.example.app.presentation.views.screens.AddProductScreen
 import com.example.app.presentation.views.screens.login
 import com.example.app.presentation.views.screens.register
 
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Navigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screens.LoginScreen.route) {
-        composable(route = Screens.LoginScreen.route) {
-            login(navController)
-        }
-        composable(route = Screens.AccountScreen.route) {
+        composable(route = Screens.ProductOverviewScreen.route) {
             ProductOverviewScreen(navController)
         }
-        composable(route = Screens.ProductOverviewScreen.route + "/{productId}",
-            arguments = listOf(
-                navArgument("productId") {
-                    nullable = false
-                    type = NavType.StringType
-                })
-        ) { entry ->
-            ProductDetailsScreen(navController, entry.arguments?.getString("productId"))
-        }
+                composable(route = Screens.ProductOverviewScreen.route + "/{productId}",
+                    arguments = listOf(
+                        navArgument("productId") {
+                            nullable = false
+                            type = NavType.StringType
+                        })
+                ) { entry ->
+                    ProductDetailsScreen(navController, entry.arguments?.getString("productId"))
+                }
         composable(route = Screens.AccountScreen.route) {
             AccountScreen(navController)
         }
         composable(route = Screens.AddProductScreen.route) {
             AddProductScreen(navController)
         }
-        composable(route = Screens.RegisterScreen.route) {
-            register(navController)
+        composable(route = Screens.LoginScreen.route) {
+            login(navController)
         }
     }
 }
